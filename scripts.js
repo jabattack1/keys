2dd5c4debdccccba8f40c41e3ea1e811804ebc9e
jabattack1 <peter.hahn@live.com>
2015-05-31 23:48:14 -0400

    c

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-// $(someElement).trigger('click');
+console.log('javascript')
+
+var note = document.getElementById("c6piano"); 
+
+
+// $( "#c6" ).mousedown(function() {
+// 	console.log('hit')
+// 	c6piano.play();
+// });
+
+
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+   
+
