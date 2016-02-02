04199028d42f119ac74a7b800f3668d24ffe90ef
jabattack1 <peter.hahn@live.com>
2016-02-02 09:21:13 +0900

    date

--- scripts.js
+++ scripts.js
@@ -20,5 +20,23 @@ function StopSound(soundobj) {
     thissound.pause();
     thissound.currentTime = 0;
 }
+
+$(document).ready(function(){
+
+  var isDown = false;   // Tracks status of mouse button
+
+  $(document).mousedown(function() {
+    isDown = true;      // When mouse goes down, set isDown to true
+  })
+  .mouseup(function() {
+    isDown = false;    // When mouse goes up, set isDown to false
+  });
+
+  $(".keys").mouseover(function(){
+    if(isDown) {        // Only change css if mouse is down
+       $(this).css({background:"#333333"});
+    }
+  });
+});
    
 
